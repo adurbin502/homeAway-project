c37fdcb1e2b5f4003451212ebf8b05e68230ae05
Andrew <a.durbin502@gmail.com>
2025-03-15 16:38:27 -0400

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+// Initialize Express application
+const app = express();
+
+// Connect essential middleware
+app.use(morgan('dev'));          // Logging
+app.use(cookieParser());         // Parse cookies
+app.use(express.json());         // Parse JSON bodies
+
+// Security Middleware
+if (!isProduction) {
+    app.use(cors());             // Enable CORS in development
+}
+
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
+);
+
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
+// Connect routes
+app.use(routes);
+
+// Error handling
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+});
+
+module.exports = app;
