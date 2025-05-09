e121b9679fbda24b8f38fb884db1afff41ac2a78
Andrew <a.durbin502@gmail.com>
2025-05-08 23:47:20 -0400

    authMe frontend login css set up

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,18 +5,16 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session'; // <-- ADD THIS LINE
 
 const store = configureStore();
 
-if (import.meta.env.MODE !== 'production') {
+if (import.meta.env.MODE !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
+  window.sessionActions = sessionActions; // <-- ADD THIS LINE
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
